ddb1d0190ddc136e8bc4bf757c4f1c28599aa71f
samwerry <league@iMac-5.attlocal.net>
2019-04-03 16:45:21 -0700

    I finished everything in the module

--- creeper/creeper.pde
+++ creeper/creeper.pde
@@ -1,3 +1,7 @@
+int x=200;
+int y=220;
+
+
 void setup(){
 size(500,500);
 
@@ -18,7 +22,28 @@ image(creeper, 176, 200);
 void draw(){
   println(mouseX);
   if(mousePressed){
-    ellipse(mouseX,mouseY,mousePressed,mousePressed);
+    ellipse(mouseX,mouseY,10,10);
+  
   }
-
+ if(isNear(mouseX,x)&&isNear(mouseY,y)){
+   fill(0,255,0);
+   
+ }
+ else{
+   fill(255,0,0);
+ }
+ if(mouseX==200&&mouseY==220){
+  println("YOU FOUND THE CREEPER, BUT FIND IT AGAIN");
+  PImage creeper;     
+creeper=loadImage("creeper.png");  
+creeper.resize(50,50);
+  image(creeper,500,500);
+ }
+ }
+ 
+boolean isNear(int a, int b) {
+if (abs(a- b) < 30)
+     return true;
+else
+     return false;
 }
--- src/extra/SimpleCalculator.java
+++ src/extra/SimpleCalculator.java
@@ -2,15 +2,19 @@ package extra;
 
 
 import javax.swing.JOptionPane;
+import javax.xml.bind.helpers.ParseConversionEventImpl;
 
 public class SimpleCalculator {
 	public static void main(String[] args) {
 
 		// 1. Get 2 numbers from the user and convert them to integer.
-
+String int1= JOptionPane.showInputDialog("Type a number");
+String int2 =JOptionPane.showInputDialog("type another number");
+int h = Integer.parseInt(int1);
+int g =Integer.parseInt(int2);
 		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
+		int operation = JOptionPane.showOptionDialog(null, "", "Choose a method", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "addition", "subraction", "multiplacation", "division" },
 				null);
 
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
@@ -23,4 +27,8 @@ public class SimpleCalculator {
 	//    HINT: use 'static void add(int num1, num2) { ... }
 
 	// 4. Create similar methods for subtraction, multiplication and division.
+}
+
+static void Add() {
+	 2 + 3 =5;
 }
